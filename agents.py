ac4e153bbd373d39c1f179709be7ca93bd035105
Simone Saletti <s.saletti@qzrstudio.com>
2023-06-21 16:30:43 +0200

    push

--- agents.py
+++ agents.py
@@ -59,12 +59,12 @@ tool = Tool(
 tool2 = Tool(
     name="UniswapV3Swap",
     func=parsing_uniswap_tool,
-    description="Execute a blockchain swap between two tokens. The input to this tool should be a comma separated list of strings of length four, representing the address of the token in input, the address of the token in output, the amount of token in input and the slippage. For example '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6,0x07865c6e87b9f70255377e024ace6630c1eaa37f,1000000000000000000,0.01' if you want to swap 1 WETH token for USDC with 1% of slippage.",
+    description="Execute a blockchain swap between two tokens. The input to this tool should be a comma separated list of strings of length four, representing the address of the token in input, the address of the token in output, the amount of token in input and the slippage. For example '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6,0x07865c6e87b9f70255377e024ace6630c1eaa37f,1000000000000000000,0.01' if you want to swap 1 WETH token for USDC with 1% of slippage. Remember that the token_in and token_out should be the addresses of the tokens on the blockchain.",
 )
 tool3 = Tool(
     name="ContractAddress",
     func=run_contract_address_tool,
-    description="Get the address of a token. The input to this tool should be a string representing the name of the token. For example 'USDC' if you want to get the address of the USDC token.",
+    description="Get the address of a token. The input to this tool should be a string representing the name of the token. For example 'USDC' if you want to get the address of the USDC token '0x07865c6e87b9f70255377e024ace6630c1eaa37f'",
 )
 
 llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613", client=None)
@@ -78,4 +78,4 @@ def call_agent(user_input):
     return agent.run(user_input)
 
 
-agent.run("Swap 0.1 WETH for USDC with 10% of slippage.")
+# agent.run("Swap 0.1 WETH for UNI with 10% of slippage.")
